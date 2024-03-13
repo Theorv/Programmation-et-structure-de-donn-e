e6c40ff816a38fd456c727fe8f12309027cb2fff
Theorv <theorv@me.com>
2024-03-13 11:47:57 +0100

    Séance 1

---
+++ TD n°1.py
@@ -0,0 +1,72 @@
+#Exercice 1
+
+# Algorithme :
+# Si la liste n'est pas rangée par ordre décroissant du mot le plus long, la trier
+# Prendre le mot le plus long et vérifier si les lettres sont disponibles
+# Recommencer pour chaque mot jusqu'à trouver le mot le plus long
+
+#Exercice 2
+
+
+
+#Functions
+
+def read_file(path):
+    # Fonction pour lire le contenu de path
+    f = open(path,"r")
+    liste = []
+    for ligne in f :
+        mot = ligne[0:len(ligne) - 1] # J'enlève les retour à la ligne
+        if len(mot) <= 8 :
+            liste.append(mot)
+    f.close()
+    return liste
+
+path = "mots.sansaccent.txt"
+mots = []
+mots = read_file(path)
+
+
+# Générer les mots possibles
+
+tirage = ['a','o','f','v','i','o','e','r']
+
+def generation(tirage):
+    mots_possibles = []
+    for mot in mots:  # Je parcours la liste de mots dans le lexique
+        A = True
+        for lettre in mot:  # Je parcours les lettres de chaque mot
+            if lettre not in tirage:
+                A = False
+        if A == True:  #  Si toutes les lettres sont dans le tirage, le mot est possible
+            mots_possibles.append(mot)
+    return mots_possibles
+
+# Garder les plus grands mots
+
+mots_possibles = generation(tirage)
+
+def plus_grand(mots_possibles):
+    candidat = mots_possibles[0]  # J'initialise le premier mot et sa taille
+    taille = len(mots_possibles[0])
+    for mot in mots_possibles:
+        if len(mot) > taille :   #  Je parcours la liste des mots pour les comparer
+            candidat = mot
+            taille = len(mot)    #  Je remplace le candidat et sa taille
+    return candidat, taille
+
+
+#Exercice 3
+
+# J'utilise la structure de tuples
+
+L1 = [(['A','E','I','L','N','O','R','S','T','U'],1)]
+L2 = [(['D','G','M'],2)]
+L3 = [(['B','C','P'],3)]
+L4 = [(['F','H','V'],4)]
+L5 = [(['J','Q'],8)]
+L6 = [(['K','W','X','Y','Z'],10)]
+
+L = L1 + L2 + L3 + L4 + L5 + L6
+
+
